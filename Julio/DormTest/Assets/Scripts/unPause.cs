9b0cc5683a5c22aba999e6c1986a48f90e4c953f
Christopher araujo <christopherjaraujo24@gmail.com>
2018-04-04 03:21:20 -0700

    Added text boxes as well as pause menu. Various issues need to be resolved including fixing NPC Text Boxes, correcting pause menu, and correcting implementation of triggers

---
+++ Julio/DormTest/Assets/Scripts/unPause.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unPause : MonoBehaviour {
+	public Transform canvas;
+	// Use this for initialization
+	public void noPause(){
+		canvas.gameObject.SetActive (false);
+		Time.timeScale = 1;
+	}
+}
---
+++ Julio/DormTest/Assets/Tresure_box/ChestOpen.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var treasureChest : GameObject; //treasure chest prefab goes here
+ 
+function OnTriggerEnter (col : Collider) {
+ 
+if(col.gameObject.tag == "Player") { //checks to see that our character controller with tag "Player" has entered the trigger
+treasureChest.animation.Play(); //plays the default animation applied to our treasureChest model
+Destroy(gameObject);// destroys the gameobject that has this script, so our collider in this case
+ 
+}
+}
